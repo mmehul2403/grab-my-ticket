79dde294fb1e0d66b53ca181c14a932ede215afb
mmehul2403 <mmehul2403@gmail.com>
2024-07-11 23:35:08 -0400

    removed commented code and added signIn and SignOut functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,49 +9,9 @@ import { useQuery, useMutation } from "@apollo/client";
 import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
-/* const NavBar = () => {
+const NavBar = () => {
   const { data, loading } = useQuery(QUERY_USER_CURRENT);
 
-  if (loading) return <p>Loading...</p>;
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">GrabMyTicket</Link>
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Movies">Movies</Link>
-        </li>
-        <li>
-          <Link to="/CreateMovie">Add Movie</Link>
-        </li>
-        <li>
-          <Link to="/AboutUs">About Us</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        {data?.currentUser ? (
-          <li>
-            <Link to="/SignOut">Sign Out</Link>
-          </li>
-        ) : (
-          <li>
-            <Link to="/SignIn">Sign In</Link>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
-
-export default NavBar;
-
- */
-const NavBar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
@@ -83,6 +43,15 @@ const NavBar = () => {
         <Button color="inherit" component={Link} to="/contact">
           Contact Us
         </Button>
+        {data?.currentUser ? (
+          <Button color="inherit" component={Link} to="/SignOut">
+            Sign Out
+          </Button>
+        ) : (
+          <Button color="inherit" component={Link} to="/SignIn">
+            Sign In
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
